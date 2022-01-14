5eb597364febd948366b1277e637b2decc706c5b
Rafael <rafalyf11@gmail.com>
2022-01-14 15:26:21 -0300

    requisito 4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Carregando from './Carregando';
 
@@ -26,6 +27,9 @@ export default class Header extends Component {
     return (
       <header data-testid="header-component">
         { loading ? <Carregando /> : <p data-testid="header-user-name">{ name }</p> }
+        <Link to="/search" data-testid="link-to-search">Buscar</Link>
+        <Link to="/favorites" data-testid="link-to-favorites">Favoritos</Link>
+        <Link to="/profile" data-testid="link-to-profile">Perfil</Link>
       </header>
     );
   }
