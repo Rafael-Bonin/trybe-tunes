5dc71ce84bb0a3a1e7be2febb3b1070baa6cce64
Rafael <rafalyf11@gmail.com>
2022-01-14 15:49:50 -0300

    requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,51 @@ import React from 'react';
 import Header from '../components/Header';
 
 export default class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      size: '',
+      able: true,
+    };
+    this.AbleButton = this.AbleButton.bind(this);
+    this.saveValue = this.saveValue.bind(this);
+  }
+
+  saveValue({ target }) {
+    const { size } = this.state;
+    this.setState({ size: target.value });
+    if (size.length >= 1) {
+      this.setState({ able: false });
+    } else {
+      this.setState({ able: true });
+    }
+  }
+
+  AbleButton(event) {
+    event.preventDefault();
+  }
+
   render() {
+    const { size, able } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            value={ size }
+            onChange={ this.saveValue }
+          />
+          <button
+            type="submit"
+            data-testid="search-artist-button"
+            onClick={ this.AbleButton }
+            disabled={ able }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
